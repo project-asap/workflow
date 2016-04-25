3796e9f29aa11e502c65eb289d0e98a0a05a989d
maxfil <maxim.filatov@unige.ch>
2016-04-25 11:45:48 +0200

    Update index.php

--- pub/php/index.php
+++ pub/php/index.php
@@ -1,4 +1,6 @@
 <?php
+# Copyright 2016 ASAP.
+#
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
